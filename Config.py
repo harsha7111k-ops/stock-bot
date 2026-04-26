3d33759c9cfcfed6b50c3a41bc83bc8d16c4e341
harsha7111k-ops <harsha7111k@gmail.com>
2026-04-26 14:16:36 +0530

    Create config.py

---
+++ Config.py
@@ -0,0 +1,16 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+STOCKS = [
+    "RELIANCE.NS",
+    "HDFCBANK.NS",
+    "ICICIBANK.NS",
+    "INFY.NS",
+    "TATAMOTORS.NS",
+    "AXISBANK.NS"
+]
+
+MIN_SCORE = 70
+TOP_N = 4
